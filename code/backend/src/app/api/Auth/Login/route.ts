9709c47d50b93aa6cbcb7ebafe8a2e82fb4658b0
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2025-12-20 23:41:41 +0700

    Wandi: Mengimpor dependensi untuk login: NextRequest, Prisma client, fungsi verifikasi password, dan pembuatan JWT token

--- code/backend/src/app/api/Auth/Login/route.ts
+++ code/backend/src/app/api/Auth/Login/route.ts
@@ -1,32 +1,4 @@
+import { NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { comparePassword } from "@/lib/hash";
 import { signToken } from "@/lib/jwt";
-
-export async function POST(req) {
-  const { email, password } = await req.json();
-
-  if (!email || !password) {
-    return Response.json({ message: "Email & password wajib" }, { status: 400 });
-  }
-
-   const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) {
-    return Response.json({ message: "User tidak ditemukan" }, { status: 404 });
-  }
-
-  const valid = await comparePassword(password, user.password);
-  if (!valid) {
-    return Response.json({ message: "Password salah" }, { status: 401 });
-  }
-
-    const token = signToken({
-    id: user.id,
-    role: user.role,
-  });
-
-    return Response.json({
-    message: "Login berhasil",
-    token,
-    role: user.role,
-  });
-}
