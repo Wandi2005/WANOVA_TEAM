660418e670a81bc3bd23ca1094893388586d92b8
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2025-12-20 17:59:46 +0700

    Wandi: Menangani request POST untuk login dengan menerima email dan password dari body request

--- code/backend/src/app/api/Auth/Login/route.ts
+++ code/backend/src/app/api/Auth/Login/route.ts
@@ -1,3 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { comparePassword } from "@/lib/hash";
 import { signToken } from "@/lib/jwt";
+
+export async function POST(req) {
+  const { email, password } = await req.json();
