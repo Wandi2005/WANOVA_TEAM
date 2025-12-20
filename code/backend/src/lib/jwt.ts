8fa78a347ede2e142def42667600bcdbfa552df5
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2025-12-20 23:24:44 +0700

    Wandi: Membuat JWT token dengan payload, secret key, dan masa berlaku 1 hari

--- code/backend/src/lib/jwt.ts
+++ code/backend/src/lib/jwt.ts
@@ -1,3 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export const signToken = (payload: object) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
+};
