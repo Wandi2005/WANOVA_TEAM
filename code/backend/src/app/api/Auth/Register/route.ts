463a874b13f02f83f9381e4e164b6c9e5c582cd9
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2025-12-20 23:28:52 +0700

    Wandi: Mengimpor dependensi untuk handler Next.js, Prisma client, dan fungsi hashing password

--- code/backend/src/app/api/Auth/Register/route.ts
+++ code/backend/src/app/api/Auth/Register/route.ts
@@ -1,34 +1,3 @@
+import { NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { hashPassword } from "@/lib/hash";
-
-export async function POST(req) {
-  const { nama, email, password } = await req.json();
-
- if (!nama || !email || !password) {
-    return Response.json({ message: "Data tidak lengkap" }, { status: 400 });
-  }
-
-   const exist = await prisma.user.findUnique({ where: { email } });
-  if (exist) {
-    return Response.json({ message: "Email sudah terdaftar" }, { status: 409 });
-  }
-
-    const hashed = await hashPassword(password);
-
-      const user = await prisma.user.create({
-    data: {
-      nama,
-      email,
-      password: hashed,
-    },
-  });
-
-    return Response.json({
-    message: "Register berhasil",
-    user: {
-      id: user.id,
-      nama: user.nama,
-      email: user.email,
-    },
-  });
-}
