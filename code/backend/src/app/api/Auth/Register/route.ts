f7f333e158d99c01a213e8a5cbf6d38f87d5fe1e
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2025-12-20 23:30:07 +0700

    Wandi: Menangani request POST untuk registrasi user dengan validasi dan error handling untuk register

--- code/backend/src/app/api/Auth/Register/route.ts
+++ code/backend/src/app/api/Auth/Register/route.ts
@@ -1,3 +1,7 @@
 import { NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { hashPassword } from "@/lib/hash";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { nama, email, password } = await req.json();
