5033e6261612cd4be1c2d500f1c34581f7077462
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2025-12-20 23:31:28 +0700

    Wandi: Validasi input registrasi: memastikan nama, email, dan password tidak kosong

--- code/backend/src/app/api/Auth/Register/route.ts
+++ code/backend/src/app/api/Auth/Register/route.ts
@@ -5,3 +5,10 @@ import { hashPassword } from "@/lib/hash";
 export async function POST(req: NextRequest) {
   try {
     const { nama, email, password } = await req.json();
+
+      if (!nama || !email || !password) {
+      return Response.json(
+        { message: "Data tidak lengkap" },
+        { status: 400 }
+      );
+    }
