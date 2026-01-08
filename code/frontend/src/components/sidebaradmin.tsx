4914bb3559f7993b9441f0df6421ac6cbd2ed5f4
naufalrapasya2 <naufalrapasya2>
2026-01-08 22:57:33 +0700

    naufal: Link ke halaman Dashboard Admin, Digunakan untuk melihat ringkasan data admin

--- code/frontend/src/components/sidebaradmin.tsx
+++ code/frontend/src/components/sidebaradmin.tsx
@@ -2,5 +2,6 @@ export default function SidebarAdmin() {
     return (
         <div className="w-60 bg-gray-200 h-screen p-4"> 
             <ul className="space-y-2">
-              <li>
-}
+             <li>
+}               <a href="/admin/dashboard">Dashboard</a>
+             </li>
