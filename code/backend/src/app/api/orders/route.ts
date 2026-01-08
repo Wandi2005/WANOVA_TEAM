f927fa5e1c8caa57203605a3b3af8b89b334932f
naufalrapasya2 <naufalrapasya2>
2026-01-08 16:12:45 +0700

    naufal: Memisahkan kata Bearer dan token

--- code/backend/src/app/api/orders/route.ts
+++ code/backend/src/app/api/orders/route.ts
@@ -7,5 +7,6 @@ export async function POST(req: Request) { try {
   const authHeader = req.headers.get("authorization");
   if (!authHeader) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
-}}
+  }}};
+
+  const token = authHeader.split(" ")[1];
