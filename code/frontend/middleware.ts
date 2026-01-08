4fd0386a614d405f8c10f675fde6564564941a82
novariodimaskusuma <novario_dimas_kusuma@teknokrat.ac.id>
2026-01-08 22:22:33 +0700

    novariodimaskusuma:Kode ini mengambil nilai token JWT dari cookie token di request masuk untuk digunakan dalam autentikasi.

--- code/frontend/middleware.ts
+++ code/frontend/middleware.ts
@@ -1,3 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
