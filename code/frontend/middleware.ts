88bd7a6912b05c280997e99bd6af758f11f13e01
novariodimaskusuma <novario_dimas_kusuma@teknokrat.ac.id>
2026-01-08 22:23:42 +0700

    novariodimaskusuma:Bagian ini memeriksa apakah pengguna belum login dengan mengecek apakah token JWT tidak ada.

--- code/frontend/middleware.ts
+++ code/frontend/middleware.ts
@@ -3,3 +3,6 @@ import type { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
+
+  // belum login
+  if (!token) {
