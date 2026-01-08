914741e8011dcf7e1354f9d4643a1095e6a28c63
naufalrapasya2 <naufalrapasya2>
2026-01-08 23:26:57 +0700

    naufal: update npm install react react-dom npm install -D @types/react @types/react-dom

--- code/frontend/src/components/sidebaradmin.tsx
+++ code/frontend/src/components/sidebaradmin.tsx
@@ -1,14 +1,14 @@
 export default function SidebarAdmin() {
     return (
-        <div className="w-60 bg-gray-200 h-screen p-4"> 
-            <ul className="space-y-2">
-             <li>
-             <a href="/admin/dashboard">Dashboard</a>
-             </li>
-             <li>
-          <a href="/admin/orders">Kelola Pesanan</a>
-        </li>
-      </ul>
-    </div>
-  );
-}
+      <div className="w-60 bg-gray-200 h-screen p-4"> 
+        <ul className="space-y-2">
+          <li>
+            <a href="/admin/dashboard">Dashboard</a>
+          </li>
+          <li>
+            <a href="/admin/orders">Kelola Pesanan</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
