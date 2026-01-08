5a6f9f7725f084075bc8482ec85b233e651ef3b1
naufalrapasya2 <naufalrapasya2>
2026-01-08 16:05:35 +0700

    naufal: update Import Verifikasi Token

--- code/backend/src/app/api/orders/route.ts
+++ code/backend/src/app/api/orders/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { verifyToken } from "@/lib/auth";
+
 
 export async function POST(req: Request) {
   const user = JSON.parse(req.headers.get("user")!);
