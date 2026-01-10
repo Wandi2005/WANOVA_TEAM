3ea5ddc8927d4ac6de267e504602b40ed5d87ee8
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2026-01-11 00:38:24 +0700

    Wandi2005 : Menambahkan fungsi logout dan styling menu sidebar berdasarkan halaman aktif

--- code/frontend/src/components/sidebaruser.tsx
+++ code/frontend/src/components/sidebaruser.tsx
@@ -6,3 +6,14 @@ import { usePathname, useRouter } from "next/navigation";
 export default function SidebarUser() {
   const pathname = usePathname();
   const router = useRouter();
+
+   const handleLogout = () => {
+    localStorage.removeItem("token");
+    router.push("/login");
+  };
+
+  const menuClass = (path: string) =>
+    `block px-4 py-2 rounded hover:bg-blue-100 ${
+      pathname === path ? "bg-blue-200 font-semibold" : ""
+    }`;
+
