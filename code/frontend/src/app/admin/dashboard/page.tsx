e267ec0a47fc61caf2a0a2170a6616e51d527f01
novariodimaskusuma <novario_dimas_kusuma@teknokrat.ac.id>
2026-01-11 01:27:22 +0700

    novariodimaskusuma:Kode tersebut digunakan untuk mengimpor komponen SidebarAdmin sebagai menu samping dan UpdateOrderModal sebagai modal untuk memperbarui data pesanan.

--- code/frontend/src/app/admin/dashboard/page.tsx
+++ code/frontend/src/app/admin/dashboard/page.tsx
@@ -1,3 +1,5 @@
 "use client";
 import { useState } from "react";
 import Navbar from "../../../components/navbar";
+import SidebarAdmin from "../../../components/sidebaradmin";
+import UpdateOrderModal from "../../../components/updateordermodal";
