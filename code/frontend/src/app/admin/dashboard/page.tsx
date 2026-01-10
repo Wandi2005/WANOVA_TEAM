2ed27ed885d00f4bd59828625715e443ad840697
novariodimaskusuma <novario_dimas_kusuma@teknokrat.ac.id>
2026-01-11 01:28:59 +0700

    novariodimaskusuma:interface Order mendefinisikan tipe data pesanan.

--- code/frontend/src/app/admin/dashboard/page.tsx
+++ code/frontend/src/app/admin/dashboard/page.tsx
@@ -3,3 +3,5 @@ import { useState } from "react";
 import Navbar from "../../../components/navbar";
 import SidebarAdmin from "../../../components/sidebaradmin";
 import UpdateOrderModal from "../../../components/updateordermodal";
+
+interface Order
