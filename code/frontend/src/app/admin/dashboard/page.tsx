c57aca5f405ca693d7912ce94f9f7e8124fd86ef
Wandi2005 <wandi_filemon_hotmartua_sianturi@teknokrat.ac.id>
2026-01-10 17:07:26 +0700

    Wandi2005 : Mendefinisikan komponen AdminDashboard yang menampilkan Navbar dengan peran ADMIN dan tata letak utama menggunakan flexbox.

---
+++ code/frontend/src/app/admin/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import Navbar from "@/components/navbar";
+import SidebarAdmin from "@/components/sidebaradmin";
+
+export default function AdminDashboard() {
+  return (
+    <>
+      <Navbar role="ADMIN" />
+      <div className="flex"></div>
