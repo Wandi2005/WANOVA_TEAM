4efd8d9e4cfbcfd11078a2565fad1224af20d99c
naufalrapasya2 <naufalrapasya2>
2026-01-10 18:12:26 +0700

    naufal: kotak modal yang akan ditampilkan di tengah overlay

--- code/frontend/src/components/updateordermodal.tsx
+++ code/frontend/src/components/updateordermodal.tsx
@@ -36,5 +36,5 @@ interface Props {
 
           return (
             <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
-
+            <div className="bg-white p-6 rounded w-96"></div>
 
