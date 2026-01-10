f5a7786489e7aa7a75046e8fda9094564086032e
naufalrapasya2 <naufalrapasya2>
2026-01-10 17:47:34 +0700

    naufal: perbaikan code sidebaradmin

--- code/frontend/src/components/sidebaradmin.tsx
+++ code/frontend/src/components/sidebaradmin.tsx
@@ -1,6 +1,5 @@
 export default function SidebarAdmin() {
     return (
-<<<<<<< HEAD
       <div className="w-60 bg-gray-200 h-screen p-4"> 
         <ul className="space-y-2">
           <li>
@@ -13,17 +12,4 @@ export default function SidebarAdmin() {
       </div>
     );
   }
-=======
-        <div className="w-60 bg-gray-200 h-screen p-4"> 
-            <ul className="space-y-2">
-             <li>
-          <a href="/admin/dashboard">Dashboard</a>
-             </li>
-             <li>
-          <a href="/admin/orders">Kelola Pesanan</a>
-        </li>
-      </ul>
-    </div>
-  );
-}
->>>>>>> 92a5ae2b67d73432ad77322b7bab255e0ea9f956
+
